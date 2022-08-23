f078b0a1a95dd3533fd214b40871bbb06b4cf009
jas112 <jas.sandmaster@gmail.com>
2022-08-22 22:09:25 -0700

    added copy-indicator elements to ColorBox js...modifided ColorBox styles.

--- src/components/colorBox/ColorBox.js
+++ src/components/colorBox/ColorBox.js
@@ -3,13 +3,33 @@ import {CopyToClipboard} from 'react-copy-to-clipboard';
 import './ColorBox.css';
 
 class ColorBox extends Component {
+    constructor(props){
+        super(props);
+        this.state = {isCopying: false};
+        this.triggerCopyIndicator = this.triggerCopyIndicator.bind(this);
+    }
+
+    triggerCopyIndicator(){
+        console.log(``);
+        this.setState({isCopying: true},() => {
+            console.log(`isCopy: ${this.state.isCopying}`);
+            setTimeout(() => {
+                this.setState({isCopying: false},() => console.log(`isCopy: ${this.state.isCopying}`));
+            }, 1500);
+        });
+    }
   render() {
     
     const {backgroundColor, name} = this.props;
 
     return (
-        <CopyToClipboard text={backgroundColor}>
+        <CopyToClipboard text={backgroundColor} onCopy={this.triggerCopyIndicator}>
             <div className='ColorBox' style={{backgroundColor: backgroundColor}}>
+                <div  className={`copy-indicator-overlay ${this.state.isCopying ? 'show' : null}`} style={{backgroundColor: backgroundColor}} />
+                <div className={`copy-indicator-msg ${this.state.isCopying ? 'show' : null}`}>
+                    <h1 className=''>copied!</h1>
+                    <p>{this.props.backgroundColor}</p>
+                </div>
                 <div className='copy-element-container'>
                     <div className='box-content'>
                         <span>{name}</span>
