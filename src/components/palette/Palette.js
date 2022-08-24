730d2d448796eeb8a990f6d4d9113576330a1571
jas112 <jas.sandmaster@gmail.com>
2022-08-24 01:42:52 -0700

    snackbar indicator of colorFormat change is functional on Palette js.

--- src/components/palette/Palette.js
+++ src/components/palette/Palette.js
@@ -2,25 +2,39 @@ import React, { Component } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import ColorBox from '../colorBox/ColorBox';
 import NavBar from '../navBar/NavBar';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 import './Palette.css';
 
 class Palette extends Component {
   constructor(props){
     super(props);
-    this.state = { level: 500 , colorFormat: 'hex'};
+    this.state = { level: 500 , colorFormat: 'hex', open: false};
     this.changeLevelValue = this.changeLevelValue.bind(this);
-    this.changeColorFormat= this.changeColorFormat.bind(this);
+    this.changeColorFormat = this.changeColorFormat.bind(this);
+    this.closeSnackBar = this.closeSnackBar.bind(this);
   }
 
   changeLevelValue(level){
     console.log(`newLevel: ${level}`);
-    this.setState({level: level})
+    this.setState({level: level});
   }
 
   changeColorFormat(evt){
     console.log(`new color format: ${evt.target.value}`);
     let newColorFormat = evt.target.value;
-    this.setState({colorFormat: newColorFormat});
+    this.setState({colorFormat: newColorFormat},() => {
+      this.setState({open: true},() => {
+        setTimeout(() => {
+          this.setState({open: false});
+        }, 3000);
+      });
+    });
+  }
+
+  closeSnackBar(){
+    this.setState({open: false});
   }
 
   getPaletteColors(level){
@@ -49,7 +63,21 @@ class Palette extends Component {
 
         {/* palette footer goes here */}
         {/* <div className='Palette-footer'></div> */}
-
+        <Snackbar 
+          anchorOrigin={{vertical: 'bottom', horizontal: 'left'}} 
+          open={this.state.open}
+          autoHideDuration={3000}
+          message={<span id='message-id'>Format Changed To:  <span style={{textTransform: 'uppercase'}}>{this.state.colorFormat}</span></span>}
+          ContentProps={{
+            'aria-describedby': 'message-id'
+          }}
+          onClose={this.closeSnackBar}
+          action={[
+            <IconButton onClick={this.closeSnackBar} color='inherit' key='close' aria-lable='close'>
+              <CloseIcon />
+            </IconButton>
+          ]}
+        />
       </div>
     )
   }
