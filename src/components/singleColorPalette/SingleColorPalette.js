a764afdc5126812bfa2604c68e26cd391d5ebebf
jas112 <jas.sandmaster@gmail.com>
2022-08-25 20:34:25 -0700

    started SingleColorPalette component.

---
+++ src/components/singleColorPalette/SingleColorPalette.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './SingleColorPalette.css';
+
+class SingleColorPalette extends Component {
+  render() {
+    return (
+        <div className='SingleColorPalette'>
+            SingleColorPalette
+        </div>
+    )
+  }
+}
+
+export default SingleColorPalette;
